beca6e64b69f56f865e961866697113c24d4257e
gasanchezm <gasm_10@icloud.com>
2026-06-23 00:04:04 +0200

    v11.1
    v11.1

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'time-for-chamba-v11.0';
+const CACHE_NAME = 'time-for-chamba-v11.1';
 const ASSETS = ['./', './index.html', './manifest.json'];
 
 self.addEventListener('install', e => {
